5f1038fe5163ca15121b0e434de2401f04d3eb3f
AgustinaGV <agustinagarciavega@gmail.com>
2020-05-07 17:02:42 -0300

    Update main.js
    Modificación en el SRC. Ahora busca en mi api backend de vercel
    Conversión de los datos de lat y lng a coma flotante

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,7 +67,7 @@ window.initMap = () => {
 //Función de asincrónica que trae los markers
 const fetchMarkers = async (map) => { 
     try {
-        const response = await fetch('assets/js/markers.json');
+        const response = await fetch('https://prog3-gv-tp2-backend.now.sh/tiendas');
         const json = await response.json();
         json.forEach(marker => {
             addMarker(map, marker);
@@ -111,7 +111,7 @@ const addMarker = (map, marker) => {
     //Agrego el marker
     const markerItem = new google.maps.Marker(
         {
-            position: { lat: lat, lng: lng },
+            position: { lat: parseFloat(lat), lng: parseFloat(lng) },
             icon: icons[type],
             map: map,
             customInfo: type
